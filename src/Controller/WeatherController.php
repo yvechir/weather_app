2b660e47ac4b7e1c430ae2d662c2e194a49a2f33
Yurii <rebelson161@gmail.com>
2025-09-27 11:47:56 +0300

    Added annotations in WeatherController.php

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -8,14 +8,23 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+/**
+ * WeatherController returns JSON or HTML response.
+ */
 final class WeatherController extends AbstractController
 {
+    /**
+     * JSON endpoint returning current weather for a given city.
+     */
     #[Route('/weather/{city}', name: 'weather_json', methods: ['GET'])]
     public function renderJson(string $city, WeatherService $weather): Response
     {
         return $this->json($weather->getWeatherData($city));
     }
 
+    /**
+     * HTML UI page with a form to query weather and show results.
+     */
     #[Route('/weather-ui', name: 'weather_ui', methods: ['GET'])]
     public function renderUi(Request $request, WeatherService $weather): Response
     {
